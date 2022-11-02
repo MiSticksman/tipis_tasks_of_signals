5368ccb74942a9e1462dad691d05e5e4c98749bf
MiSticksman <vadim02101@gmail.com>
2022-11-02 14:11:50 +0300

    added filtering of the synthesized signal

--- Shoforostov(tipis2)/main.py
+++ Shoforostov(tipis2)/main.py
@@ -10,24 +10,28 @@ if __name__ == '__main__':
     modulation_names = ["am", "fm", "pm"]
     spectrum_names = ["am spectrum", "fm spectrum", "pm spectrum"]
 
+    freq = 2
+    amplitude = 3
+
     plt.figure(figsize=(16, 10))
 
-    freq = 2
-    amplitude = 10
-
     am = amplitude_modulation(freq=freq, amplitude=amplitude)
-    ams_x, ams_y = amplitude_modulation_spectrum(freq=freq)
+    ams_x, ams_y = modulation_spectrum(amplitude_modulation, freq=freq)
 
     fm = frequency_modulation(freq=freq)
-    fms_x, fms_y = frequency_modulation_spectrum(freq)
+    fms_x, fms_y = modulation_spectrum(frequency_modulation, freq)
 
     pm = phase_modulation(freq=freq)
-    pms_x, pms_y = phase_modulation_spectrum(freq=freq)
+    pms_x, pms_y = modulation_spectrum(phase_modulation, freq=freq)
 
     modulations = [am, fm, pm]
 
     spectra_x = [ams_x, fms_x, pms_x]
     spectra_y = [ams_y, fms_y, pms_y]
+
+    images = 'images'
+    if not os.path.isdir(images):
+        os.mkdir(images)
 
     for i in range(3):
         plt.subplot(2, 3, i + 1)
@@ -43,84 +47,30 @@ if __name__ == '__main__':
         plt.xlabel('freq')
         plt.xlim(0, 30)
 
-    modulations = "modulations"
-    if not os.path.isdir(modulations):
-        os.mkdir(modulations)
-    plt.savefig(modulations + '/modulation.jpg')
+    plt.savefig(images + '/modulations.jpg')
 
     plt.figure(figsize=(16, 10))
 
-    synthesis = synthesis_of_amplitude_modulation_signal()
-    plt.subplot(4, 1, 1)
-    t1 = np.arange(0, 1, 1 / len(synthesis))
-    plt.plot(t1, synthesis)
+    yf_clean = spectrum_with_main_frequencies()
+    plt.subplot(2, 2, 1)
+    plt.plot(ams_x, np.abs(yf_clean))
+    plt.xlim(0, 25)
 
-    # yf_clean = spectrum_with_fundamental_frequencies()
-    # plt.subplot(4, 1, 2)
-    # plt.plot(ams_x, np.abs(yf_clean))
-    # plt.xlim(0, 30)
-
-    f_clean = s_o_a_m_s()
-    plt.subplot(4, 1, 2)
+    f_clean = synthesis_of_am_signal()
+    plt.subplot(2, 2, 2)
     plt.plot(TIME, f_clean)
 
-    meander = unipolar_meander(freq, amplitude)
-    meander[0] = 0
-    plt.subplot(4, 1, 3)
+    meander = unipolar_meander(freq, amplitude=1)
+    # meander[0] = 0
+    plt.subplot(2, 2, 3)
     plt.plot(TIME, meander)
 
-    filtering = filtering_of_synthesized_am_signal()
-    filt = ex()
-    plt.subplot(4, 1, 4)
+    filtering = execute_filter_signal(synthesis_of_am_signal)
+    plt.subplot(2, 2, 4)
     t2 = np.arange(0, 1, 1 / len(filtering))
-    plt.plot(TIME, filt)
+    plt.plot(TIME, filtering)
+
+    plt.savefig(images + '/synthesisAndFiltration.jpg')
 
     plt.show()
-
-
-
-# TODO обрезать am спектр (оставить три основные: 8, 10, 12 частоты), провести синтез сигнала (получится отдаленный модулирванный сигнал), затем провести фильтрацию этого сигнала (должен получиться менадр)
-
-    # plt.subplot(2, 3, 1)
-    # plt.plot(TIME, am, 'r')
-    # plt.title("am")
-    # plt.ylabel('amplitude')
-    # plt.xlabel('time')
-    #
-    #
-    # plt.subplot(2, 3, 4)
-    # plt.plot(ams_x, ams_y, 'r')
-    # plt.title("am spectrum")
-    # plt.ylabel('power')
-    # plt.xlabel('freq')
-    # plt.xlim(0, 30)
-
-    # plt.subplot(2, 3, 2)
-    # plt.plot(TIME, fm, 'g')
-    # plt.title("fm")
-    # plt.ylabel('amplitude')
-    # plt.xlabel('time')
-    #
-    #
-    # plt.subplot(2, 3, 5)
-    # plt.plot(fms_x, fms_y, 'g')
-    # plt.title("fm spectrum")
-    # plt.ylabel('power')
-    # plt.xlabel('freq')
-    # plt.xlim(0, 30)
-    #
-    #
-    # plt.subplot(2, 3, 3)
-    # plt.plot(TIME, pm, 'b')
-    # plt.title("pm")
-    # plt.ylabel('amplitude')
-    # plt.xlabel('time')
-    #
-    #
-    # plt.subplot(2, 3, 6)
-    # plt.plot(pms_x, pms_y, 'b')
-    # plt.title("pm spectrum")
-    # plt.ylabel('power')
-    # plt.xlabel('freq')
-    # plt.xlim(0, 30)
 
--- Shoforostov(tipis2)/modulation.py
+++ Shoforostov(tipis2)/modulation.py
@@ -1,8 +1,8 @@
-from numpy.fft import fft, ifft, fftfreq, rfftfreq, rfft, irfft
+from numpy import sign
+from numpy.fft import rfftfreq, rfft, irfft
 from scipy.signal import butter, filtfilt
 
 from constants import *
-from scipy import signal
 
 
 def unipolar_meander(freq=2, amplitude=1):
@@ -10,56 +10,20 @@ def unipolar_meander(freq=2, amplitude=1):
 
 
 def amplitude_modulation(freq=2, amplitude=1):
-    # meandr = amplitude * np.sin(2 * np.pi * freq * t)
-    # for i in range(len(meandr)):
-    #     if meandr[i] > 0:
-    #         meandr[i] = amplitude
-    #     else:
-    #         meandr[i] = 0
     fc = 10
     carrier = 1 * np.cos((2 * np.pi * fc * TIME) + np.pi / 2)  # несущий высокочастотный сигнал
-    # product1 = carrier * (1 + meandr)
     modulator = unipolar_meander(freq, amplitude)  # информационный сигнал
     am = carrier * (1 + modulator)
     return am
 
 
-def amplitude_modulation_spectrum(freq=2):
-    y = amplitude_modulation(freq)
-    n = len(y)
-    # frq = np.arange(n)
-    # frq = frq[range(int(n))]
-    frq = rfftfreq(n, 1 / FS)
-    yf = rfft(y)  # деление на n - нормализация
-    # yf = (np.abs(s1) ** 2) / n
-    # yf = yf[range(int(n))]
-    yf[0] = 0
-    return frq, yf
-
-
 def frequency_modulation(freq=2):
     carrier = 1 * np.cos((2 * np.pi * freq * TIME))  # base signal
-    # phi = (CARRIER_FREQ * TIME) + (freq * carrier)
-    # fm = np.sin(np.pi * phi)
-
-    # fm = 1 * np.cos(2 * np.pi * fc * TIME + (5 * 1/freq) * np.sin(freq * TIME))
-
     fc = 10  # carrier frequency
     k = 0.05  # sensitivity
     phi = 2 * np.pi * fc * TIME + k * np.cumsum(carrier)  # phase
     fm = np.cos(phi)  # modulated signal
     return fm
-
-
-def frequency_modulation_spectrum(freq=2):
-    y = frequency_modulation(freq)
-    n = len(y)
-    frq = rfftfreq(n, 1 / FS)
-    yf = rfft(y)
-    # yf = (np.abs(s1) ** 2) / n
-    # yf = yf[range(int(n))]
-    yf[0] = 0
-    return frq, yf
 
 
 def phase_modulation(freq=2):
@@ -71,51 +35,70 @@ def phase_modulation(freq=2):
     return pm
 
 
-def phase_modulation_spectrum(freq=2):
-    y = phase_modulation(freq)
+def modulation_spectrum(modulation, freq=2):
+    y = modulation(freq)
     n = len(y)
     frq = rfftfreq(n, 1 / FS)
-    yf = rfft(y)
-    # yf = (np.abs(s1) ** 2) / n
-    # yf = yf[range(int(n))]
+    yf = rfft(y)  # деление на n - нормализация
     yf[0] = 0
     return frq, yf
 
 
-def synthesis_of_amplitude_modulation_signal():
-    x, yf = amplitude_modulation_spectrum()
-    new_map = dict(sorted(dict(zip(yf, x)).items(), reverse=True))
-    # y = list(new_map.keys())
-    # for i in range(3, len(y)):
-    #     y[i] = 0
+# def amplitude_modulation_spectrum(freq=2):
+#     y = amplitude_modulation(freq)
+#     n = len(y)
+#     # frq = np.arange(n)
+#     # frq = frq[range(int(n))]
+#     frq = rfftfreq(n, 1 / FS)
+#     yf = rfft(y)  # деление на n - нормализация
+#     # yf = (np.abs(s1) ** 2) / n
+#     # yf = yf[range(int(n))]
+#     yf[0] = 0
+#     return frq, yf
 
-    # yf = sorted(yf, reverse=True)
-    # for i in range(3, len(yf)):
-    #     yf[i] = 0
-    # x = list(new_map.values())[:3]
+
+# def frequency_modulation_spectrum(freq=2):
+#     y = frequency_modulation(freq)
+#     n = len(y)
+#     frq = rfftfreq(n, 1 / FS)
+#     yf = rfft(y)
+#     yf[0] = 0
+#     return frq, yf
+
+
+# def phase_modulation_spectrum(freq=2):
+#     y = phase_modulation(freq)
+#     n = len(y)
+#     frq = rfftfreq(n, 1 / FS)
+#     yf = rfft(y)
+#     yf[0] = 0
+#     return frq, yf
+
+
+def synthesis_of_amplitude_modulation_signal():
+    _, yf = modulation_spectrum(amplitude_modulation)
     new_sig = irfft(yf)
     return new_sig
 
-def spectrum_with_fundamental_frequencies():
-    _, yf = amplitude_modulation_spectrum()
+
+def spectrum_with_main_frequencies():
+    _, yf = modulation_spectrum(amplitude_modulation)
     yf_abs = np.abs(yf)
     index = yf_abs > 50
     yf_clean = index * yf
     return yf_clean
 
-def s_o_a_m_s():
-    yf_clean = spectrum_with_fundamental_frequencies()
+
+def synthesis_of_am_signal():
+    yf_clean = spectrum_with_main_frequencies()
     f_clean = irfft(yf_clean)
     return f_clean
 
 
-def filtering_of_synthesized_am_signal():
-    y = synthesis_of_amplitude_modulation_signal()
-    new_sig = ifft(y)
-    return new_sig
-
-def ex():
-    y = s_o_a_m_s()
-    b, a = butter(3, 0.9)
-    filtered = filtfilt(b, a, y)
+def execute_filter_signal(synthesized_signal):
+    y = synthesized_signal()
+    filtered = []
+    b, a = butter(4, 0.008)
+    for i in filtfilt(b, a, y):
+        filtered.append((sign(i) + 1) / 2)
     return filtered
